4dd551392006417dd9b694c03661008a444b1519
Choreas <niklas.kosanke@web.de>
2020-09-06 16:16:34 +0200

    eslint setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 module Main {
-	document.addEventListener("deviceready", onDeviceReady, false);
+	document.addEventListener('deviceready', onDeviceReady, false);
 	//	100% of the browser window - see Boot.js for additional configuration
-	var game = new Phaser.Game({ width: "100%", height: "100%", type: Phaser.AUTO });
+	var game = new Phaser.Game({ width: '100%', height: '100%', type: Phaser.AUTO });
 	function onDeviceReady() {
-		game.scene.add("Boot", Scenes.Boot);
+		game.scene.add('Boot', Scenes.Boot);
 		game.scene.add('MainMenu', Scenes.MainMenu);
 		game.scene.start('Boot');
 	}
--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -1,16 +1,15 @@
-module Scenes {
-    export class Boot extends Phaser.Scene {
-        constructor() {
-            super('Boot');
-        }
-        init(): void {
-        }
-        preload(): void {
-            this.load.image('starfield', './assets/images/starfield.png');
-        }
-        create(): void {
-            this.scene.start('MainMenu');
-
-        }
-    }
+namespace Scenes {
+	export class Boot extends Phaser.Scene {
+		constructor() {
+			super('Boot');
+		}
+		init(): void {
+		}
+		preload(): void {
+			this.load.image('starfield', './assets/images/starfield.png');
+		}
+		create(): void {
+			this.scene.start('MainMenu');
+		}
+	}
 }
